7929992582da9799d4b360f650ed3509263aac84
gautier kiss <gkgkgk395@gmail.com>
2021-04-27 15:08:27 +0200

    yes

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 # include "./ft_ping.h"
 
+int  fd = 0, nb_packet_sended, nb_packed_received;
+void                *ptr;
+struct addrinfo     hints;
+struct addrinfo     *res;
+char                addrstr[100];
+uint32_t            type;
+
 uint16_t    checksum(uint16_t *msg, uint32_t size) {
     uint32_t sum = 0;
     uint16_t check = 0;
@@ -10,16 +17,75 @@ uint16_t    checksum(uint16_t *msg, uint32_t size) {
     return ~check;
 }
 
-int     main(int argc, char **argv) {
-    if (argc == 1)
+static void end(int signal) {
+
+}
+
+static void send_ping() {
+    uint8_t buff[ICMP_SIZE + TIME_SIZE];
+
+    // SIZE = 28
+    struct icmp test;
+    // SIZE = 16    
+    struct timeval tv;
+    // SIZE = 8
+    struct timezone tz;
+
+    test.icmp_type = 8;
+    test.icmp_code = 0;
+    test.icmp_cksum = 0;
+    test.icmp_hun.ih_idseq.icd_id = getpid();
+    test.icmp_hun.ih_idseq.icd_seq = nb_packet_sended;
+    ++nb_packet_sended;
+
+    gettimeofday(&tv, &tz);
+
+    memcpy(buff, &test, ICMP_SIZE);
+    memcpy(&buff[ICMP_SIZE], &tv, TIME_SIZE);
+    test.icmp_cksum = checksum((uint16_t *)buff, sizeof(buff) / 2);
+    memcpy(buff, &test, ICMP_SIZE);
+
+    int x = sendto(fd, buff, ICMP_SIZE + TIME_SIZE, 0, (struct sockaddr*)ptr, sizeof(struct sockaddr));
+    if (x < 0) {
+        printf("Error while sending package: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
+    }
+}
 
-    struct addrinfo     hints;
-    struct addrinfo     *res;
-    void                *ptr;
-    char                addrstr[100];
-    uint32_t            type;
+static void receive_ping() {
+    uint8_t    msg_buffer[256];
+    memset(msg_buffer, 0, sizeof(msg_buffer));
 
+    struct iovec iov[1];
+    memset(iov, 0, sizeof(iov));
+    iov[0].iov_base = msg_buffer;
+    iov[0].iov_len = sizeof msg_buffer ;
+
+    struct msghdr       msg;
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_name = NULL; //optional address (void*)
+    msg.msg_namelen = 0; // address size (socklen_t)
+    msg.msg_iov = iov; //table scatter/gather (struct iovec)
+    msg.msg_iovlen = 0; // len of msg_iov (size_t)
+    msg.msg_control = NULL; // metadata (void *)
+    msg.msg_controllen = 0; //size of ... (socklen_t)
+    msg.msg_flags = 0; // atributes of msg received (int);
+
+    int z = recvmsg(fd, &msg, 0);
+    if (z < 0) {
+        printf("Error %d while receiving package: %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void pingou_pingouin(int signal) {
+    signal = 0;
+    send_ping();
+    //signal(SIGALRM, pingou_pingouin);
+    alarm(1);
+}
+
+static void init_socket(char **argv) {
     res = malloc(sizeof(struct addrinfo));
     
     hints.ai_flags = 0;
@@ -47,14 +113,14 @@ int     main(int argc, char **argv) {
         if (type == AF_INET) {
             inet_ntop (res->ai_family, &((struct sockaddr_in *)ptr)->sin_addr, addrstr, 100);
         } else {
-            inet_ntop (res->ai_family, &((struct sockaddr_in6 *)ptr)->sin6_addr, addrstr, 100);
+            inet_ntop (res->ai_family, &((struct sockaddr_in *)ptr)->sin_addr, addrstr, 100);
         }
         
         printf ("IPv%d address: %s (%s)\n", res->ai_family == PF_INET6 ? 6 : 4, addrstr, res->ai_canonname);
         res = res->ai_next;
     }
     
-    int fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+    fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (fd < 0) {
         printf("Error while init socket: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
@@ -62,56 +128,18 @@ int     main(int argc, char **argv) {
 
     int ttl = 118; //nb max of touterbefore getting destroyed
     setsockopt(fd, IPPROTO_IP, IP_TTL, &ttl, sizeof ttl);
+}
 
-    uint8_t buff[ICMP_SIZE + TIME_SIZE];
+int     main(int argc, char **argv) {
+    if (argc == 1)
+        exit(EXIT_FAILURE);
 
-    // SIZE = 28
-    struct icmp test;
-    // SIZE = 16    
-    struct timeval tv;
-    // SIZE = 8
-    struct timezone tz;
-
-    test.icmp_type = 8;
-    test.icmp_code = 0;
-    test.icmp_cksum = 0;
-    test.icmp_hun.ih_idseq.icd_id = getpid();
-    test.icmp_hun.ih_idseq.icd_seq = 0;
-
-    gettimeofday(&tv, &tz);
-
-    memcpy(buff, &test, ICMP_SIZE);
-    memcpy(&buff[ICMP_SIZE], &tv, TIME_SIZE);
-    test.icmp_cksum = checksum((uint16_t *)buff, sizeof(buff) / 2);
-    memcpy(buff, &test, ICMP_SIZE);
-
-    int x = sendto(fd, buff, ICMP_SIZE + TIME_SIZE, 0, (struct sockaddr*)ptr, sizeof(struct sockaddr));
-    if (x < 0) {
-        printf("Error while sending package: %s\n", strerror(errno));
-    }
-
-    uint8_t    msg_buffer[256];
-    memset(msg_buffer, 0, sizeof(msg_buffer));
-
-    struct iovec iov[1];
-    memset(iov, 0, sizeof(iov));
-    iov[0].iov_base = msg_buffer;
-    iov[0].iov_len = sizeof msg_buffer ;
-
-    struct msghdr       msg;
-    memset(&msg, 0, sizeof(msg));
-    msg.msg_name = NULL; //optional address (void*)
-    msg.msg_namelen = 0; // address size (socklen_t)
-    msg.msg_iov = iov; //table scatter/gather (struct iovec)
-    msg.msg_iovlen = 0; // len of msg_iov (size_t)
-    msg.msg_control = NULL; // metadata (void *)
-    msg.msg_controllen = 0; //size of ... (socklen_t)
-    msg.msg_flags = MSG_PEEK; // atributes of msg received (int);
-
-    int z = recvmsg(fd, &msg, MSG_PEEK);
-    if (z < 0) {
-        printf("Error %d while receiving package: %s\n", errno, strerror(errno));
-    }
+    init_socket(argv);
+    send_ping();
+    receive_ping();
 
     exit(EXIT_SUCCESS);
+
+    signal(SIGQUIT, end);
+    signal(SIGALRM, pingou_pingouin);
 }
