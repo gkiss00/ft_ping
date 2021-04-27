4b25265b31973373910d4c47506b46222b54e5de
gautier kiss <gkgkgk395@gmail.com>
2021-04-27 18:10:03 +0200

    functional

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 # include "./ft_ping.h"
 
 int                 fd = 0, nb_packet_sended, nb_packed_received;
-double              min, max, avg, stddev;
+double              min = 1000, max = -1, sumary = 0, avg, stddev;
 void                *ptr;
 struct addrinfo     hints;
 struct addrinfo     *res;
 char                addrstr[100];
 uint32_t            type;
+int ttl = 118; //nb max of touterbefore getting destroyed
+char *domain;
 
 uint16_t    checksum(uint16_t *msg, uint32_t size) {
     uint32_t sum = 0;
@@ -20,36 +22,37 @@ uint16_t    checksum(uint16_t *msg, uint32_t size) {
 
 static void end(int signal) {
     double pct = 1 - (double)((double)nb_packed_received / (double)nb_packet_sended);
-    printf("--- %s ping statistics ---\n");
-    printf("%d packets transmitted, %d packets received, %f%% packet loss", nb_packet_sended, nb_packed_received, pct);
-    printf("round-trip min/avg/max/stddev = %f/%f/%f/%f ms");
+    pct *= 100;
+    printf("--- %s ping statistics ---\n", domain);
+    printf("%d packets transmitted, %d packets received, %.01f%% packet loss\n", nb_packet_sended, nb_packed_received, pct);
+    printf("round-trip min/avg/max/stddev = %.03f/%.03f/%.03f/%d ms\n", min, max, sumary / nb_packet_sended, 0);
+    exit(EXIT_SUCCESS);
 }
 
 static void send_ping() {
-    uint8_t buff[ICMP_SIZE + TIME_SIZE];
+    uint8_t buff[64];
+    memset(buff, 0, 64);
 
     // SIZE = 28
     struct icmp test;
     // SIZE = 16    
     struct timeval tv;
-    // SIZE = 8
-    struct timezone tz;
 
     test.icmp_type = 8;
     test.icmp_code = 0;
     test.icmp_cksum = 0;
-    test.icmp_hun.ih_idseq.icd_id = getpid();
-    test.icmp_hun.ih_idseq.icd_seq = nb_packet_sended;
+    test.icmp_id = getpid();
+    test.icmp_seq = nb_packet_sended;
     ++nb_packet_sended;
 
-    gettimeofday(&tv, &tz);
+    gettimeofday(&tv, NULL);
 
     memcpy(buff, &test, ICMP_SIZE);
     memcpy(&buff[ICMP_SIZE], &tv, TIME_SIZE);
     test.icmp_cksum = checksum((uint16_t *)buff, sizeof(buff) / 2);
     memcpy(buff, &test, ICMP_SIZE);
 
-    int x = sendto(fd, buff, ICMP_SIZE + TIME_SIZE, 0, (struct sockaddr*)ptr, sizeof(struct sockaddr));
+    int x = sendto(fd, buff, 64, 0, (struct sockaddr*)ptr, sizeof(struct sockaddr));
     if (x < 0) {
         printf("Error while sending package: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
@@ -70,15 +73,37 @@ static void receive_ping() {
     msg.msg_name = NULL; //optional address (void*)
     msg.msg_namelen = 0; // address size (socklen_t)
     msg.msg_iov = iov; //table scatter/gather (struct iovec)
-    msg.msg_iovlen = 0; // len of msg_iov (size_t)
+    msg.msg_iovlen = 1; // len of msg_iov (size_t)
     msg.msg_control = NULL; // metadata (void *)
     msg.msg_controllen = 0; //size of ... (socklen_t)
     msg.msg_flags = 0; // atributes of msg received (int);
 
-    int z = recvmsg(fd, &msg, 0);
-    if (z < 0) {
-        printf("Error %d while receiving package: %s\n", errno, strerror(errno));
-        exit(EXIT_FAILURE);
+    while(1){
+        int z = recvmsg(fd, &msg, 0);
+        if (z < 0) {
+            printf("Error %d while receiving package: %s\n", errno, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        struct icmp *response;   
+        response = (struct icmp *)&msg_buffer[20];
+
+        if (response->icmp_type == ICMP_ECHOREPLY && response->icmp_id == getpid()) {
+            struct timeval tv;
+            gettimeofday(&tv, NULL);
+            struct timeval *tv2 = (struct timeval *)&msg_buffer[20 + ICMP_SIZE];
+            double diff = (tv.tv_sec - tv2->tv_sec) * UINT32_MAX + (tv.tv_usec - tv2->tv_usec);
+            diff /= 1000;
+            sumary += diff;
+            if (min > diff)
+                min = diff;
+            if (max < diff)
+                max = diff;
+            ++nb_packed_received;
+            printf("64 bytes from %s: icmp_seq=%d ttl=%d time=%.03f ms\n", addrstr, nb_packet_sended - 1, ttl, diff);
+        } else {
+            printf("SHIT\n");
+        }
     }
 }
 
@@ -130,7 +155,7 @@ static void init_socket(char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    int ttl = 118; //nb max of touterbefore getting destroyed
+    
     setsockopt(fd, IPPROTO_IP, IP_TTL, &ttl, sizeof ttl);
 }
 
@@ -138,12 +163,14 @@ int     main(int argc, char **argv) {
     if (argc == 1)
         exit(EXIT_FAILURE);
 
+    domain = argv[1];
     init_socket(argv);
-    send_ping();
-    receive_ping();
-
-    exit(EXIT_SUCCESS);
-
-    signal(SIGQUIT, end);
+    printf("PING %s (%s): 56 data bytes\n", argv[1], addrstr);
+    
+    signal(SIGINT, end);
     signal(SIGALRM, pingou_pingouin);
+
+    send_ping();
+    alarm(1);
+    receive_ping();
 }
